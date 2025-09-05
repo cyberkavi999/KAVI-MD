f2bd24f97a7b995815333452151ec4497bde707d
kavishka dilshan <cyberkavishka@gmail.com>
2025-09-05 10:26:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // session id  
+SESSION_ID: process.env.SESSION_ID || "xnkllRBZ#15zyqj96EN9IYum4-DmGz4rZQouDybNifHXLUNqT4EM", // session id  
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public", //public , groups , inbox , channel
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
